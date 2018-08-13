2b107fe5bc6c3482e0925a7b012cc467c79a47de
Mazen <mazenayoub96@hotmail.com>
2018-08-13 14:53:13 +0200

    Experiment with Form4

--- BlueBerry/Form4.cs
+++ BlueBerry/Form4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace BlueBerry
 {
@@ -48,6 +49,11 @@ namespace BlueBerry
 			
 
 			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
